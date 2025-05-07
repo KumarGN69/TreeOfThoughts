a8c36de6954b3ebc4d26945ca4c24f23ee43c80c
KumarGN69 <kumargn1969@hotmail.com>
2025-05-07 22:49:59 +0000

    changes to take the query from api call

--- routes.py
+++ routes.py
@@ -1,16 +1,16 @@
 from tree_of_thoughts import TreeOfThoughts
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 tree_of_thoughts = TreeOfThoughts()
 @app.route('/ideas', methods=['GET'])
 def getidea():
-    user_input = input(f"Enter your idea for brainstorming :")
+    user_input = request.args.get('query')
     ideas = tree_of_thoughts.getidea(user_input)
-    ideas_list = [idea.name for idea in ideas.solutions]
-    # return jsonify(ideas_list)
-    return render_template("index.html", ideas=ideas_list)
+    thoughts = [{"name":idea.name, "description":idea.description} for idea in ideas.solutions]
+    print(thoughts)
+    return render_template("index.html", ideas = thoughts, thought=user_input)
 
 if __name__ == '__main__':
     app.run(debug=True)
