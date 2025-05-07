6d90f1340b0723c16c1c8d9bfb5e66f09684c28e
kumarGN69 <kumargn1969@otmail.com>
2025-05-07 15:40:47 -0500

    Initial changes

---
+++ routes.py
@@ -0,0 +1,16 @@
+from tree_of_thoughts import TreeOfThoughts
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+tree_of_thoughts = TreeOfThoughts()
+@app.route('/')
+def getidea():
+    user_input = input(f"Enter your idea for brainstorming :")
+    ideas = tree_of_thoughts.getidea(user_input)
+    ideas_list = [idea.name for idea in ideas.solutions]
+    # return jsonify(ideas_list)
+    return render_template("index.html", ideas=ideas_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
