4b70b2659ceab1b05e51b9c1f9ad7357c78feae8
kumarGN69 <kumargn1969@otmail.com>
2025-05-07 21:02:46 -0500

    changes to enter a query for brainstorming

--- routes.py
+++ routes.py
@@ -9,8 +9,18 @@ def getidea():
     user_input = request.args.get('query')
     ideas = tree_of_thoughts.getidea(user_input)
     thoughts = [{"name":idea.name, "description":idea.description} for idea in ideas.solutions]
-    print(thoughts)
+    # print(thoughts)
     return render_template("index.html", ideas = thoughts, thought=user_input)
+
+@app.route('/submit', methods=['POST'])
+def submit_text():
+    text = request.form['text']
+    ideas = tree_of_thoughts.getidea(text)
+    thoughts = [{"name":idea.name, "description":idea.description} for idea in ideas.solutions]
+    return render_template("index.html", ideas = thoughts, thought=text)
+    # Process the text as needed
+    # return f'Text received: {text}'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
